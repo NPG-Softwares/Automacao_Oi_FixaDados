316c8b654711e6c6d4b6a2a38946154eaff1f99a
Nycolas Galdino <nycolaspimentel12@gmail.com>
2024-10-11 16:38:58 -0300

    feat: Add new endpoints, send log function and new get accounts method

--- app/Objects/Obj_ApiSpring.py
+++ app/Objects/Obj_ApiSpring.py
@@ -3,8 +3,6 @@ import dotenv
 import requests as req
 from typing import Literal
 from functools import cache
-from datetime import datetime as dt
-from datetime import timedelta as td
 
 from Objects.Obj_ApiSpringBase import BaseClient, BaseInvoice
 
@@ -20,6 +18,14 @@ class LoginError(Exception):
     pass
 
 
+class UploadError(Exception):
+    pass
+
+
+class API_Spring_Error(Exception):
+    pass
+
+
 # ------------------------------------------------- Classes
 class API_Spring:
     _token = {}
@@ -28,12 +34,12 @@ class API_Spring:
         self.ambient = ambient
         self.__get_envs__()
 
-        if API_Spring._token.get(self.ambient):
+        if API_Spring._token.get(self.ambient, None):
             self.token = API_Spring._token.get(self.ambient)
 
         else:
             self.token = API_Spring._token.get(self.ambient) or self.get_token()
-            API_Spring._token.setdefault(self.ambient, self.token)
+            API_Spring._token[self.ambient] = self.token
 
     def __get_envs__(self) -> None:
         if self.ambient == 'hml':
@@ -41,9 +47,11 @@ class API_Spring:
             self.password = os.getenv('hml_login_password')
             self.end_token = os.getenv('hml_end_get_token')
             self.end_logins = os.getenv('hml_end_logins')
-            self.end_get_invoices = os.getenv('hml_end_get_invoices')
             self.end_up_invoice = os.getenv('hml_end_up_invoice')
+            self.end_get_invoices = os.getenv('hml_end_get_invoices')
+            self.end_send_error_log = os.getenv('hml_end_send_error_log')
             self.end_get_fornecedores_id = os.getenv('hml_end_get_fornecedores_id')
+            self.end_get_unidades_by_id = os.getenv('hml_end_get_unidades_by_id')
 
         elif self.ambient == 'prod':
             self.email = os.getenv('prod_login_email')
@@ -52,14 +60,16 @@ class API_Spring:
             self.end_logins = os.getenv('prod_end_logins')
             self.end_up_invoice = os.getenv('prod_end_up_invoice')
             self.end_get_invoices = os.getenv('prod_end_get_invoices')
+            self.end_send_error_log = os.getenv('prod_end_send_error_log')
             self.end_get_fornecedores_id = os.getenv('prod_end_get_fornecedores_id')
+            self.end_get_unidades_by_id = os.getenv('prod_end_get_unidades_by_id')
 
         else:
             raise AmbientError('Ambiente inválido, utilize os ambientes '
                                '[hml | prod] sendo "hml" (homologação) ou '
                                '"prod" (produção).')
 
-    def get_logins(self, fornecedor_id: str = None) -> dict:
+    def get_logins(self, fornecedor_id: str = None, cliente_id: str = None) -> dict:
         url = self.end_logins
         headers = {}
         headers['Authorization'] = self.token
@@ -68,6 +78,9 @@ class API_Spring:
 
         if fornecedor_id:
             params['fornecedorId'] = fornecedor_id
+
+        if cliente_id:
+            params['clienteId'] = cliente_id
 
         r = req.get(url, headers=headers, params=params)
 
@@ -83,7 +96,7 @@ class API_Spring:
 
         r = req.get(url, headers=headers, params=params)
 
-        return [x['id'] for x in r.json()['data']]
+        return [(x['id'], x['clienteAdmId']) for x in r.json()['data']]
 
     @cache
     def get_token(self) -> str:
@@ -111,17 +124,13 @@ class API_Spring:
             raise LoginError(f'Erro [{r.status_code}]: {r.text}')
 
     def get_accounts(self, client: BaseClient) -> dict:
-        now = dt.now()
         url = self.end_get_invoices
         headers = {}
         headers['Authorization'] = self.token
         headers['Status'] = 'PENDENTE'
 
         params = {}
-        params['clientId'] = client.cliente_id
-        params['fornecedorId'] = client.fornecedor_id
-        params['vencimentoMinimo'] = now.strftime('%Y-%m-%d')
-        params['vencimentoMaximo'] = (now + td(days=60)).strftime('%Y-%m-%d')
+        params['ClienteId'] = client.cliente_id
 
         r = req.get(url, headers=headers, params=params)
 
@@ -165,8 +174,35 @@ class API_Spring:
                 raise req.exceptions.RequestException(f'Erro [{r.status_code}]: {r.text}')
         print('Fatura enviada com sucesso.')
 
+    def send_error_log(self, title: str, message: str, stacktrace: str,
+                       origin: str, status: Literal['warning', 'error', 'info', 'success'] = 'error',
+                       login_id: int = 1, login_create_id: int = None,
+                       cliente_adm_id: int = None) -> None:
+        url = self.end_send_error_log
+        headers = {}
+        headers['Authorization'] = self.token
 
-def filter_logins(api: API_Spring, logins: dict) -> list[BaseClient]:
+        payload = {
+            'titulo': title,
+            'mensagem': message,
+            'infoCode': stacktrace,
+            'origem': origin,
+            'status': status,
+            'login_id': login_id,
+            'login_create_id': login_create_id,
+            'cliente_adm_id': cliente_adm_id
+        }
+
+        r = req.post(url, headers=headers, json=payload)
+
+        if r.status_code == 200:
+            print('Log enviado com sucesso!')
+        else:
+            print('Payload:', payload)
+            raise UploadError(f'Erro [{r.status_code}]: {r.text}')
+
+
+def filter_logins(api: API_Spring, logins: dict, fornecedores_ids: list[int]) -> list[BaseClient]:
     filtered_accounts: list[str] = []
     filtered_logins: list[BaseClient] = []
 
@@ -183,11 +219,15 @@ def filter_logins(api: API_Spring, logins: dict) -> list[BaseClient]:
             continue
 
         client = BaseClient(login)
+        accounts = []
         accounts = api.get_accounts(client)['data']
 
         for account in accounts:
             obj_account = BaseInvoice(account)
             if obj_account.numero_conta in filtered_accounts:
+                continue
+
+            if obj_account.fornecedor_id not in fornecedores_ids:
                 continue
 
             client.contas.append(obj_account)
@@ -214,11 +254,36 @@ def get_logins(name: str, ambient: Literal['prod', 'hml'] = 'prod') -> list[Base
     api = API_Spring(ambient)
     fornecedores_ids = api.get_fornecedores_by_name(name)
     logins = []
-    for fornecedor_id in fornecedores_ids:
-        logins += api.get_logins(fornecedor_id)['data']
+    list_clientes_ids = list(set([x for _, x in fornecedores_ids]))
+    list_fornecedores_ids = list(set([x for x, _ in fornecedores_ids]))
+    for cliente_id in list_clientes_ids:
+        new_logins = []
+        all_logins = api.get_logins(cliente_id=cliente_id)['data']
+        for login in all_logins:
+            if login['fornecedorId'] in list_fornecedores_ids:
+                new_logins.append(login)
 
-    logins = filter_logins(api, logins)
+        logins.extend(new_logins)
+
+    logins = filter_logins(api, logins, list_fornecedores_ids)
     return logins
+
+
+def send_log(ambient: Literal['prod', 'hml'], title: str, message: str, stacktrace: str,
+             origin: str, status: Literal['warning', 'error', 'info', 'success'] = 'error',
+             login_id: int = 1, login_create_id: int = None,
+             cliente_adm_id: int = None):
+    api = API_Spring(ambient)
+    api.send_error_log(
+        title=title,
+        message=message,
+        stacktrace=stacktrace,
+        origin=origin,
+        status=status,
+        login_id=login_id,
+        login_create_id=login_create_id,
+        cliente_adm_id=cliente_adm_id
+    )
 
 
 if __name__ == '__main__':
